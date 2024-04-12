ef9b189158640dbc785b44e2b97f686f2f4d4ec7
willC707 <91046237+willC707@users.noreply.github.com>
2024-04-12 17:44:43 -0400

    Add files via upload

--- webpage/myscripts.js
+++ webpage/myscripts.js
@@ -1,4 +1,4 @@
-let username = "";
+var oldMessage, mostRecentMessage;
 
 function Connect() {
    username = document.getElementById("username").value;
@@ -6,6 +6,7 @@ function Connect() {
 }
 
 function SendMsg(ele) {
+   let username = "Owen";
    if(event.key === 'Enter' || event.button === 0) {
       const msg = document.getElementById("text").value;
       const please = document.querySelector('.message');
@@ -15,24 +16,36 @@ function SendMsg(ele) {
       var newMessage = document.createElement("li");
       newMessage.innerHTML = `
       <li id="chatbox" style="margin-bottom:15px; font-size: 125%;" class="most-recent">
-         <p id="most-recent-message">${msg}</p>
+         <div class="usernameAndMsg">
+            <p class="user">${username}</p>
+            <p id="most-recent-message">${msg}</p>
+         </div>
          <img class="sent" src="images/user-icon.png" width="40px">
       </li>
       `
-      message.appendChild(newMessage);
-      console.log(username);
+      message.append(newMessage);
    }
 }
 
 function receiveMsg() {
    msg = "This is a test..."
+   username = "AI"
    var newMessage = document.createElement("li");
    newMessage.innerHTML = `
    <li id="chatbox" style="margin-bottom:15px; font-size: 125%;" class="r-most-recent">
       <img class="r-sent" src="images/user-icon.png" width="40px">
-      <p id="most-recent-message">${msg}</p>
+      <div class="r-usernameAndMsg">
+         <p class="user">${/*sentUsername*/username}</p>
+         <p id="most-recent-message">${msg}</p>
+      </div>
    </li>
    `
 
-   message.appendChild(newMessage);
+   message.append(newMessage);
+   storeText(msg);
+}
+
+function storeText(message) {
+   mostRecentMessage = oldMessage
+   message = mostRecentMessage
 }
