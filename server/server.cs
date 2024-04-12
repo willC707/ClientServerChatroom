1b086d5f0444fb21a270fcf15fc4ec60e2acb3bd
willC707 <will.coleman707@gmail.com>
2024-04-12 19:54:44 -0400

    added Leave for leaving board group

--- server/server.cs
+++ server/server.cs
@@ -73,9 +73,12 @@ public class ChatServer
             //Read incoming message from client and convert to string
             int received = clientSocket.Receive(buffer);
             string message = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine(message);
 
             //Split the command from the content
+            //TODO Error handle for missing command split
             string[] parts = message.Split(new char[] {' '}, 2);
+            //Console.WriteLine(parts.Count);
 
             string command = parts[0];
             string content = parts[1];
@@ -223,7 +226,8 @@ public class ChatServer
                 string boardList = "All Boards: ";
                 foreach (string boardName in m_boards.Keys)
                 {
-                    boardList += boardList + "\n";
+                    boardList += boardName + "\n";
+                    
                 }
 
                 messageBytes = Encoding.ASCII.GetBytes(boardList);
@@ -238,6 +242,18 @@ public class ChatServer
 
                 m_mutex.WaitOne();
 
+                if (m_boards.ContainsKey(currentBoard))
+                {
+                    if(m_boardMembers[currentBoard].Contains(new Tuple<string, bool>(username, true)))
+                    {
+                        int index = m_boardMembers[currentBoard].FindIndex(t => t.Equals(new Tuple<string, bool>(username, true)));
+                        if (index != -1)
+                        {
+                            m_boardMembers[currentBoard][index] = new Tuple<string, bool>(username, false);
+                        }
+                    }
+                }
+
                 if ( m_boards.ContainsKey(boardName))
                 {
                     bool partOfGroup = false;
@@ -246,39 +262,76 @@ public class ChatServer
                         if(user.Item1 == username)
                         {   
                             partOfGroup = true;
-                            Tuple<string,bool> userTuple = new Tuple<string, bool>(username, false);
-                            int index = m_boardMembers[boardName].FindIndex(t => t.Equals(userTuple));
-
-                            if (index != -1)
-                            {
-                                userTuple = new Tuple<string, bool>(username, true);
-                                m_boardMembers[boardName][index] = userTuple;
-
-                                currentBoard = boardName;
-
-                                messageBytes = Encoding.ASCII.GetBytes("[SERVER] You are now logged into: " + boardName);
-                                clientSocket.Send(messageBytes);
-
-                                foreach (Tuple<string, bool> boardUser in m_boardMembers[boardName])
-                                {
-                                    messageBytes = Encoding.ASCII.GetBytes("[SERVER] User " + username + " Joined board " + boardName);
-
-                                    if (boardUser.Item1 != username && boardUser.Item2)
-                                    {
-                                        m_clients[boardUser.Item1].Send(messageBytes);
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                messageBytes = Encoding.ASCII.GetBytes("ERROR: You are already on this board");
-                                clientSocket.Send(messageBytes);
-                            }
                         }
                         
                     }
+                    if (partOfGroup)
+                    {
+                        Tuple<string,bool> userTuple = new Tuple<string, bool>(username, false);
+                        int index = m_boardMembers[boardName].FindIndex(t => t.Equals(userTuple));
 
-                    if (!partOfGroup)
+                        if (index != -1)
+                        {
+                            userTuple = new Tuple<string, bool>(username, true);
+                            m_boardMembers[boardName][index] = userTuple;
+
+                            currentBoard = boardName;
+
+                            messageBytes = Encoding.ASCII.GetBytes("[SERVER] You are now logged into: " + boardName);
+                            clientSocket.Send(messageBytes);
+
+                            foreach (Tuple<string, bool> boardUser in m_boardMembers[boardName])
+                            {
+                                messageBytes = Encoding.ASCII.GetBytes("[SERVER] User " + username + " Joined board " + boardName);
+
+                                if (boardUser.Item1 != username && boardUser.Item2)
+                                {
+                                    m_clients[boardUser.Item1].Send(messageBytes);
+                                }
+                            }
+
+                            if(m_boards[currentBoard].Count >=2)
+                            {
+                                Message msg1 = m_boards[currentBoard][m_boards[currentBoard].Count - 2];
+                                Message msg2 = m_boards[currentBoard][m_boards[currentBoard].Count - 1];
+
+                                string messageString = "\n\nMessage ID: " + msg1.Id + 
+                                    "\nSender: " + msg1.Sender + "\nPost Date: " + msg1.PostDate + 
+                                    "\nSubject: " + msg1.Subject + "\nContent: " + msg1.Content;
+                                    
+                                
+                                messageBytes = Encoding.ASCII.GetBytes(messageString);
+                                clientSocket.Send(messageBytes);
+
+                                messageString = "\n\nMessage ID: " + msg2.Id + 
+                                    "\nSender: " + msg2.Sender + "\nPost Date: " + msg2.PostDate + 
+                                    "\nSubject: " + msg2.Subject + "\nContent: " + msg2.Content;
+                                    
+                                
+                                messageBytes = Encoding.ASCII.GetBytes(messageString);
+                                clientSocket.Send(messageBytes);
+                            }
+                            else
+                            {
+                                foreach (Message msgn in m_boards[currentBoard])
+                                {
+                                    string messageString = "\n\nMessage ID: " + msgn.Id + 
+                                        "\nSender: " + msgn.Sender + "\nPost Date: " + msgn.PostDate + 
+                                        "\nSubject: " + msgn.Subject + "\nContent: " + msgn.Content;
+                                    
+                                
+                                    messageBytes = Encoding.ASCII.GetBytes(messageString);
+                                    clientSocket.Send(messageBytes);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            messageBytes = Encoding.ASCII.GetBytes("ERROR: You are already on this board");
+                            clientSocket.Send(messageBytes);
+                        }
+                    }
+                    else
                     {
                         messageBytes = Encoding.ASCII.GetBytes("ERROR: You are not a part of the user group");
                         clientSocket.Send(messageBytes);
@@ -356,6 +409,25 @@ public class ChatServer
                 clientSocket.Shutdown(SocketShutdown.Both);
                 clientSocket.Close();
                 break;
+            }
+            else if (command == "LEAVE")
+            {
+                // Add function to leave a board group
+                string groupName = content;
+
+                m_mutex.WaiteOne();
+
+                if (m_boardMembers.ContainsKey(groupName))
+                {
+                    if(m_boardMembers[groupName].Contains(new Tuple<string, bool>(username, false)) || m_boardMembers[groupName].Contains(new Tuple<string, bool>(username, true)))
+                    {
+                        int index = m_boardMembers[groupName].FindIndex(t => t.Item1 == username && (t.Item2 == true || t.Item2 == false));
+                        if (index != -1)
+                        {
+                            m_boardMembers[board].RemoveAt(index);
+                        }
+                    }   
+                }
             }
             else
             {
